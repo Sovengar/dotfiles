5ff7707e0de629f39aa93853ed9c5f3387470edb
Sovengar <sovengar@users.noreply.github.com>
2026-05-17 14:35:29 +0200

    chore: split shell-plugins from shells

---
+++ linux/setup/packaging/shell-plugins/all.sh
@@ -0,0 +1,17 @@
+# Shell plugins all.sh
+
+if [[ -z "${_GUARDS_LOADED:-}" ]]; then
+  SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+  source "$SCRIPT_DIR/../../helpers/all.sh"
+fi
+
+_PHASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+run_logged "$_PHASE_DIR/starship.sh"
+run_logged "$_PHASE_DIR/gum.sh"
+run_logged "$_PHASE_DIR/fastfetch.sh"
+run_logged "$_PHASE_DIR/bash-completion.sh"
+run_logged "$_PHASE_DIR/bat.sh"
+run_logged "$_PHASE_DIR/eza.sh"
+run_logged "$_PHASE_DIR/gotty.sh"
+run_logged "$_PHASE_DIR/snip.sh"
--- linux/setup/packaging/shells/all.sh
+++ linux/setup/packaging/shells/all.sh
@@ -7,14 +7,6 @@ fi
 
 _PHASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-run_logged "$_PHASE_DIR/starship.sh"
-run_logged "$_PHASE_DIR/gum.sh"
-run_logged "$_PHASE_DIR/fastfetch.sh"
-run_logged "$_PHASE_DIR/bash-completion.sh"
 run_logged "$_PHASE_DIR/zsh.sh"
 run_logged "$_PHASE_DIR/fish.sh"
 run_logged "$_PHASE_DIR/nushell.sh"
-run_logged "$_PHASE_DIR/bat.sh"
-run_logged "$_PHASE_DIR/eza.sh"
-run_logged "$_PHASE_DIR/gotty.sh"
-run_logged "$_PHASE_DIR/snip.sh"
