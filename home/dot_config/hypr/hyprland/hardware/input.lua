2f3f8d53fabb421e86c16eaf99642583e5eb32e5
Sovengar <sovengar@users.noreply.github.com>
2026-05-22 00:09:54 +0200

    chore(hypr): increase keyboard repeat rate to 100 and lower delay to 150

---
+++ home/dot_config/hypr/hyprland/hardware/input.lua
@@ -0,0 +1,21 @@
+-- Input variables: https://wiki.hypr.land/Configuring/Variables/#input
+
+hl.config({
+    input = {
+        -- follow_mouse = 1,
+        -- sensitivity = 0,
+        -- force_no_accel = 0,
+        accel_profile = "flat",
+        numlock_by_default = true,
+        kb_layout = "es",
+        -- ms to wait before a held key starts repeating.
+        repeat_delay = 150,
+        -- Repetitions per second while a key is held.
+        repeat_rate = 100,
+
+        -- Touchpad variables: https://wiki.hypr.land/Configuring/Variables/#touchpad
+        touchpad = {
+            natural_scroll = false,
+        },
+    },
+})
