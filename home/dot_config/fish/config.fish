7a4ccb650d1888b9ee6f3db6f068e50b67482d95
Sovengar <sovengar@users.noreply.github.com>
2026-06-18 23:48:40 +0200

    improvements

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -1,4 +1,1 @@
 # Fish automatically loads ~/.config/fish/conf.d/*.fish.
-
-# ── Quality of Life ───────────────────────────────────────────────────────────
-abbr --add reload 'source ~/.config/fish/config.fish'
